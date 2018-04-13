a18e8690907843e7bbb8c12f3b69cdeab664bfc6
Peter Kroon <peterkroon06+github@gmail.com>
2018-04-13 11:27:51 -0700

    Improve test output

--- tests/test_read_smiles.py
+++ tests/test_read_smiles.py
@@ -25,12 +25,46 @@ class Tests(unittest.TestCase):
     def assertEqualGraphs(self, graph1, graph2):
         out = nx.is_isomorphic(graph1, graph2,
                                node_match=operator.eq, edge_match=operator.eq)
-        if not out:
-            simple_out = nx.is_isomorphic(graph1, graph2)
-            if not simple_out:
-                raise AssertionError("Graphs not isomorphic")
-            else:
-                raise AssertionError('Node or edge attributes do no match')
+        if out:
+            return
+
+        matcher = nx.isomorphism.GraphMatcher(graph1, graph2)
+        matches = list(matcher.isomorphisms_iter())
+        if not matches:
+            raise AssertionError("Graphs not isomorphic")
+
+        # Now for the hard part. Find the isomorphism that matches best, and
+        # then figure out where the attributes differ.
+        scores = []
+        for m_idx, match in enumerate(matches):
+            score = 0
+            for node_idx, node_jdx in match.items():
+                if graph1.nodes[node_idx] == graph2.nodes[node_jdx]:
+                    score += 1
+            for idx1, jdx1 in graph1.edges:
+                idx2, jdx2 = match[idx1], match[jdx1]
+                edge1 = graph1.edges[idx1, jdx1]
+                edge2 = graph2.edges[idx2, jdx2]
+                if edge1 == edge2:
+                    score += 1
+            scores.append((score, m_idx))
+
+        # This one should be the best.
+        _, m_idx = max(scores)
+        match = matches[m_idx]
+        for node_idx, node_jdx in match.items():
+            self.assertEqual(graph1.nodes[node_idx], graph2.nodes[node_jdx])
+        for idx1, jdx1 in graph1.edges:
+            idx2, jdx2 = match[idx1], match[jdx1]
+            edge1 = graph1.edges[idx1, jdx1]
+            edge2 = graph2.edges[idx2, jdx2]
+            if edge1 == edge2:
+                score += 1
+            if edge1 != edge2:
+                fmt = 'Edge between {} and {} is not equal: {} is not {}'
+                raise AssertionError(fmt.format(graph1.nodes[idx1],
+                                                graph1.nodes[jdx1],
+                                                edge1, edge2))
 
     def test_single_chain(self):
         smiles = 'CCCC'
